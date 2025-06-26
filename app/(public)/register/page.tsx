64bab036d02040dc4c4aeb912a94d231fcbe0514
Everton <evertoniee@yahoo.com.br>
2025-06-26 02:13:42 +0000

    feature: add next auth

--- app/(public)/register/page.tsx
+++ app/(public)/register/page.tsx
@@ -3,6 +3,7 @@ import RegisterForm from './RegisterPageClient';
 import { getServerSession } from 'next-auth';
 import { redirect } from 'next/navigation';
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { Metadata } from 'next';
 
 export const metadata = {
     title: 'Log Chat - Register',
